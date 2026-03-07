e5940b19cae1a6eec6f0d7b5f9eda12c43941e12
quapka <quapka@gmail.com>
2026-03-07 15:36:26 +0100

    Save application data to default path
    Previously, the data got installed to wherever the application was
    installted. This, however, does prevent running the application when
    installed at read-only filesystem locations, such is the case for NixOS.

--- lib/util/app_dir_getter.dart
+++ lib/util/app_dir_getter.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart' as path_pkg;
 
 class AppDirGetter {
   static Future<Directory> getAppDir() async {
@@ -13,10 +12,6 @@ class AppDirGetter {
       return getApplicationSupportDirectory();
     }
 
-    final path = path_pkg.join(
-      path_pkg.dirname(Platform.resolvedExecutable),
-      'app',
-    );
-    return Directory(path);
+    return getApplicationDocumentsDirectory();
   }
 }
